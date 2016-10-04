0602e938b41e5ed975302381cfe5f002bda77fa4
mehmetalikoker <mehmetalikoker@outlook.com>
2016-10-04 19:47:25 +0300

    Business and Business.Contracts Layers is done

---
+++ OnlineStore.Business.Contracts/IUserEngine.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Business.Contracts
+{
+    public interface IUserEngine : IBusinessEngine
+    {
+    }
+}
---
+++ OnlineStore.Business/BusinessEngineBase.cs
@@ -0,0 +1,50 @@
+﻿using OnlineStore.Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Business
+{
+    public class BusinessEngineBase
+    {
+        public IConfigurationHelper ConfigurationHelper;
+        private readonly ILogger _logger;
+
+        public BusinessEngineBase()
+        {
+            // TODO: DI Framework eklendikden sonra bu field initialize edilecek..
+        }
+
+        protected T ExecuteWithExceptionHandledOperation<T>(Func<T> func)
+        {
+            try
+            {
+                var result = func.Invoke();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message, ex);
+
+                throw;
+            }
+        }
+
+        protected void ExecuteWithExceptionHandledOperation<T>(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message, ex);
+
+                throw;
+            }
+        }
+    }
+}
